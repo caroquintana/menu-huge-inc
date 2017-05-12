b94b81cb834107f96ba935831aca67d0129f236b
caroquintana <caro.quintana.n@gmail.com>
2017-05-11 21:52:58 -0400

    correcciones parte1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
 document.getElementById("toggle").addEventListener("click", function (event) {
 	event.preventDefault();
-	document.getElementById("header").classList.toggle("open");
+	document.getElementById("nav-header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden");
 
 });
-
-
+var currentScroll = window.pageYOffset || document.body.scrollTop;
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
 	var currentScroll = window.pageYOffset || document.body.scrollTop;
 	if(currentScroll > lastScrollTop){
 		console.log("down");
+	}else{
+		console.log("up");
+	}	
+}, false);
+/*
 	} else{
 		document.getElementById("nav-header").classList.add("header-in");
 		document.getElementById("nav-header").classList.add("solid");
@@ -19,5 +23,4 @@ window.addEventListener("scroll", function(){
 		document.getElementById("nav-header").classList.remove("solid");
 		document.getElementById("nav-header").classList.remove("header-in");
 	}
-
-}, false);
+*/
